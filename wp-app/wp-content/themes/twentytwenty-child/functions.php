2b737607647d76c44d17c22f80ab1e0f3caa0369
Konrad Bolek <konrad.bolek@polcode.net>
2024-03-23 14:22:19 +0100

    Part3 Create user and disable admin bar

--- wp-app/wp-content/themes/twentytwenty-child/functions.php
+++ wp-app/wp-content/themes/twentytwenty-child/functions.php
@@ -7,3 +7,14 @@
         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     }
     add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+    /**
+     * Remove wp admin bar for `wp-test` user
+     */
+    $current_user_login = wp_get_current_user()->user_login;
+    if ( $current_user_login === 'wp-test' ) {
+        add_filter( 'show_admin_bar', '__return_false' );
+    }
+
+    // Create user if not exist
+    require get_stylesheet_directory() . '/inc/create-user.php';
---
+++ wp-app/wp-content/themes/twentytwenty-child/inc/create-user.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * Create user if not exists with editor role
+     */
+    $user_id = wp_create_user('wp-test', '123456789', 'wptest@elementor.com');
+    if(is_wp_error($user_id)){
+        $error = $user_id->get_error_message();
+    }else{
+        $user_id_role = new WP_User($user_id);
+        $user_id_role->set_role('editor');
+    }
